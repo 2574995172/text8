78bdf282ebceb1ec9036cf79969de84aea3618d5
lianglin <2574995172@qq.com>
2022-06-09 19:54:54 +0800

    v20220611

--- src/com/Main.java
+++ src/com/Main.java
@@ -9,5 +9,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("234567");
         System.out.println("9999");
+        System.out.println("345678");
     }
 }
